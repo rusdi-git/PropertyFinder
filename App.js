f916d7c6dd64a757614b425e3eaabd2ce914ee61
Unknown <rusditoriq101@gmail.com>
2019-05-17 06:56:13 +0700

    Create Home

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 'use strict';
 import React, {Component} from 'react';
-import { Platform,StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, 
+   } from 'react-native';
+import {createAppContainer,createStackNavigator} from 'react-navigation';
+import SearchPage from './SearchPage';
 
 
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return <Text style={styles.description}>Search for houses to buy!</Text>;
-  }
-}
-
-const styles = StyleSheet.create({
-  description: {
-    fontSize:18,
-    textAlign:'center',
-    color:'#656565',
-    marginTop:65,
-  }
+const AppNavigator = createStackNavigator({
+  Home:{screen:SearchPage},
 });
+const App = createAppContainer(AppNavigator);
+export default App;
